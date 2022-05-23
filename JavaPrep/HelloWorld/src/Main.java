760cd875a8aa6fd3c1b138971b5fac97813dc856
Priyansusahoo <Priyansusahoo1@gmail.com>
2022-05-23 17:13:04 +0530

    Modified: Challenge Added

--- JavaPrep/HelloWorld/src/Main.java
+++ JavaPrep/HelloWorld/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        /**
+         * Challenge - to print our "Hello <your-name>"
+         */
+        System.out.println("Hello, Priyansu");
     }
 }
