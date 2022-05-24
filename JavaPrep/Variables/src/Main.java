fe08753275040cc8354162eac55e39950860b4f9
Priyansusahoo <Priyansusahoo1@gmail.com>
2022-05-24 12:17:34 +0530

    Added: Variables

---
+++ JavaPrep/Variables/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+
+        /**
+         * Variables
+         */
+        int myFirstNumber = 5;
+        /**
+         * challenge 1 - to print the value of <myFirstNumber>
+         */
+        System.out.println(myFirstNumber);
+
+    }
+}
