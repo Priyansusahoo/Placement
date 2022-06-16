159c8f50d36452304f7b4bcc368d861dbc957c51
Priyansusahoo <Priyansusahoo1@gmail.com>
2022-06-16 13:14:45 +0530

    Added : Challenge

---
+++ JavaPrep/challenge1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        byte a = 10;
+        short b = 20;
+        int c = 50;
+        long d = 50000 + (10 * (a + b + c));
+        System.out.println(d);
+    }
+}
