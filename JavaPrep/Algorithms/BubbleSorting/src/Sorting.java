2fcb548f7c819aa299a810442f720b9ba7d3102e
Priyansusahoo <Priyansusahoo1@gmail.com>
2022-07-28 10:24:22 +0530

    Added: Algorithm -> BubbleSorting

---
+++ JavaPrep/Algorithms/BubbleSorting/src/Sorting.java
@@ -0,0 +1,30 @@
+public class Sorting {
+
+    public static void printArray(int arr[]) {
+        for(int i=0;i< arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+
+        /**
+         * Bubble Sort
+         * time complexity = O(n^2) (not good)
+         * constants are ignored while calculating TC
+         */
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j=0;j<arr.length -i - 1;j++){
+                if (arr[j] > arr[j+1]){
+                    //Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
