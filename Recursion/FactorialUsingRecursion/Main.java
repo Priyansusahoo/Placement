e396a3fd255b4cec15b8094e30b53f9572b3263d
priyansusahoo <priyansusahoo1@gmail.com>
2022-08-31 23:31:56 +0530

    Added: FactorialUsingRecursion

---
+++ Recursion/FactorialUsingRecursion/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+	public static void main(String[] args){
+		Main recursion = new Main();
+		var rec = recursion.factorial(4);
+		System.out.println(rec);
+	}
+	
+	public int factorial(int n){
+		if (n<0){
+			return -1;
+		}
+		if (n == 0 || n == 1){
+			return 1;
+		}
+		return n * factorial(n-1);
+	}
+
+}
