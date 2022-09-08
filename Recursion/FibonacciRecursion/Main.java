76d797a2361efef9861c732fda60d7f28da53118
priyansusahoo <priyansusahoo1@gmail.com>
2022-09-08 23:48:07 +0530

    Modified:  changes in working directory)

--- Recursion/FibonacciRecursion/Main.java
+++ Recursion/FibonacciRecursion/Main.java
@@ -1,1 +1,24 @@
+class Main{
+	public static void main(String[] args){
+		Main recursion = new Main();
+		var rec = recursion.fibonacci(3);
+		System.out.println(rec);
+	}
 
+
+	/**
+	 * lang. StackOverflowError is a runtime error
+	 * It  indicates that the application stack is
+	 * exhausted and is usually caused by deep or infinite recursion.
+	 */
+	public int fibonacci(int n){
+		if (n == 0 || n == 1){
+			return n;
+		}
+		if (n<0){
+			return -1;
+		}
+		return fibonacci(n-1) + fibonacci(n-2);
+	}
+
+}
