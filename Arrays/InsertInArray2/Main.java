8b3b8e006dd62f32d0d1199c1528140836e51abf
Priyansusahoo <Priyansusahoo1@gmail.com>
2022-09-17 23:07:33 +0530

    Backup code --> TODO later

---
+++ Arrays/InsertInArray2/Main.java
@@ -0,0 +1,10 @@
+class Main{
+	public static void main(String[] args){
+		SingleDimensionArray sda = new SingleDimensionArray(10);
+		sda.insert(0,0);
+		sda.insert(1,10);
+		sda.insert(2,20,);
+		sda.insert(1,30);
+		sda.insert(12,120);
+	}
+}
