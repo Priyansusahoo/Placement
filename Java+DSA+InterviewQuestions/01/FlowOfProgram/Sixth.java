e71c94414ad1d5fadd820ae99dc2c276a54c8715
Priyansusahoo <priyansusahoo1@gmail.com>
2022-09-20 09:29:35 +0530

    Added: Input currency in rupees and output in USD.

---
+++ Java+DSA+InterviewQuestions/01/FlowOfProgram/Sixth.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;;
+
+public class Sixth {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int rupees = input.nextInt();
+        input.close();
+
+        float USD = (float) (rupees / 79.67);
+        System.out.println(String.format("%.02f", USD)
+                + " USD");
+    }
+}
