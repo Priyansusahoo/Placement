569185f0c3551352d18ef3ef16bf7ec9d1aecf9e
Priyansusahoo <priyansusahoo1@gmail.com>
2022-09-22 16:20:17 +0530

    Added: BubbleOWN.java

---
+++ Java+DSA+InterviewQuestions/01/FlowOfProgram/BubbleOWN.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BubbleOWN {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // int[] arr = getIntegers(0);
+        int[] arr = { 6, 7, 5, 2, 6, 1 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j + 1] > arr[j]) { // change ">" to "<" for ascending-order.
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArr(arr);
+
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    // public static int[] getIntegers(int numbers) {
+    // System.out.println("Enter how many elements u want in the array ");
+    // numbers = scanner.nextInt();
+    // System.out.println("Enter " + numbers + " elements\r");
+    // int[] values = new int[numbers];
+    // for (int i = 0; i < values.length; i++) {
+    // values[i] = scanner.nextInt();
+    // }
+    // return values;
+    // }
+}
