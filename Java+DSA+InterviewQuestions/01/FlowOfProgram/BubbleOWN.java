6679a715643920d0310202da9fa8229aef06c2af
Priyansusahoo <priyansusahoo1@gmail.com>
2022-09-24 12:47:50 +0530

    Modified: Optimized-> BubbleOWN.java

--- Java+DSA+InterviewQuestions/01/FlowOfProgram/BubbleOWN.java
+++ Java+DSA+InterviewQuestions/01/FlowOfProgram/BubbleOWN.java
@@ -4,8 +4,8 @@ public class BubbleOWN {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // int[] arr = getIntegers(0);
-        int[] arr = { 6, 7, 5, 2, 6, 1 };
+        int[] arr = getIntegers();
+        // int[] arr = { 6, 7, 5, 2, 6, 1 };
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j + 1] > arr[j]) { // change ">" to "<" for ascending-order.
@@ -26,14 +26,14 @@ public class BubbleOWN {
         }
     }
 
-    // public static int[] getIntegers(int numbers) {
-    // System.out.println("Enter how many elements u want in the array ");
-    // numbers = scanner.nextInt();
-    // System.out.println("Enter " + numbers + " elements\r");
-    // int[] values = new int[numbers];
-    // for (int i = 0; i < values.length; i++) {
-    // values[i] = scanner.nextInt();
-    // }
-    // return values;
-    // }
+    public static int[] getIntegers() {
+        System.out.println("Enter how many elements u want in the array ");
+        int numbers = scanner.nextInt();
+        System.out.println("Enter " + numbers + " elements\r");
+        int[] values = new int[numbers];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
 }
