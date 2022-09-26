2476559a0bc6072c7927dd2cb47f497fd87b9dd1
Priyansusahoo <priyansusahoo1@gmail.com>
2022-09-26 07:04:43 +0530

    Diffrence BTW equals() and ==

---
+++ TEST/Main.java
@@ -0,0 +1,14 @@
+class Main{ 
+public static void main(String[] args) {
+
+        String a = "Priyansu";
+        String b = "priyansu";
+
+        if (a == b) {
+            System.out.println("true");
+        }
+        if (a != b) {
+            System.out.println("False");
+        }
+    }
+}
