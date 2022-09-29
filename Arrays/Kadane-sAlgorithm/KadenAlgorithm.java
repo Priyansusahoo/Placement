2223283f6b30bc95634d088316f30b739e9dd756
Priyansusahoo <priyansusahoo1@gmail.com>
2022-09-29 11:11:22 +0530

    Added: KadenAlgorithm.java

---
+++ Arrays/Kadane-sAlgorithm/KadenAlgorithm.java
@@ -0,0 +1,28 @@
+//maximumSumSubarray
+class KadenAlgorithm {
+    public static void main(String[] args) {
+        KadenAlgorithm max = new KadenAlgorithm();
+        int[] a = { 5, -4, -2, 6, -1 };
+        int[] ary = { -4, -2, -1 };
+        max.maxSumSequence(a);
+        max.maxSumSequence(ary);
+        System.out.println(max.maxSumSequence(a));
+        System.out.println(max.maxSumSequence(ary));
+    }
+
+    public int maxSumSequence(int[] array) {
+        int maxSum = 0;
+        int curSum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            curSum += array[i];
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+            if (curSum < 0) {
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
