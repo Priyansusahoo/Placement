6ae86c1020cd703d01a098f1ef4aa41dc53a13de
Priyansusahoo <priyansusahoo1@gmail.com>
2022-09-30 15:57:54 +0530

    Added: CodingExercise43

---
+++ Java+DSA+InterviewQuestions/01/FlowOfProgram/ReverseNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+        input.close();
+        int temp = number;
+        int reverse = 0, rimender;
+
+
+    }
+}
