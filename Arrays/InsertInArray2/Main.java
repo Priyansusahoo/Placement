ce961559f2e0feb311fd7853839f5695227318b4
Priyansusahoo <priyansusahoo1@gmail.com>
2022-10-07 21:01:48 +0530

    Added: SingleDimensionArray

--- Arrays/InsertInArray2/Main.java
+++ Arrays/InsertInArray2/Main.java
@@ -1,10 +1,17 @@
 class Main {
 	public static void main(String[] args) {
-		SingleDimensionArray sda = new SingleDimensionArray(10);
-		sda.insert(0, 0);
-		sda.insert(1, 10);
-		sda.insert(2, 20);
-		sda.insert(1, 30);
-		sda.insert(12, 120);
+		SingleDimensionArray objectOfSDA = new SingleDimensionArray(12);
+		// insert elements in array
+		objectOfSDA.insert(0, 0);
+		objectOfSDA.insert(1, 10);
+		objectOfSDA.insert(2, 20);
+		objectOfSDA.insert(1, 30);
+		objectOfSDA.insert(11, 120);
+
+		// search Element in the array
+		objectOfSDA.searchElementInArray(120);
+		// delete an element
+		objectOfSDA.deleteElement(11);
+		objectOfSDA.deleteElement(12);
 	}
 }
--- Arrays/InsertInArray2/SingleDimensionArray.java
+++ Arrays/InsertInArray2/SingleDimensionArray.java
@@ -17,7 +17,7 @@ public class SingleDimensionArray {
                         + " with value "
                         + value);// O(1)
             } else {
-                System.out.println("Not Success - The index"
+                System.out.println("Not Success - The index "
                         + index
                         + " is not empty");// O(1)
             }
@@ -25,8 +25,51 @@ public class SingleDimensionArray {
             System.out.println(index
                     + " is a invalid index");// O(1)
         }
+        /**
+         * Time-Complexity for insert: O(1)
+         */
+    }
+
+    // Array Traversal
+    public void traverseArray() {
+        try {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        } catch (Exception e) {
+            System.out.println("Array no longer exists");
+        }
+    }
+
+    // Search for an element in the given Array
+    // Linear-Search
+    public void searchElementInArray(int element) {
+
+        for (int i = 0; i < arr.length; i++) {// O(N)
+            if (arr[i] == element) {// O(1)
+                System.out.println("Element found at index " + i); // O(1)
+                return;
+            }
+        }
+        System.out.println(element + " not found"); // O(1)
+
+        /**
+         * Time-Complexity for searchElementInArray :- O(N)
+         * Space-Complexity :- O(1)
+         */
+    }
+
+    // delete element in the array
+    public void deleteElement(int deleteElementIndex) {
+        try {
+            arr[deleteElementIndex] = Integer.MIN_VALUE; // O(1)
+            System.out.println("Index " + deleteElementIndex + " deleted successfully"); // O(1)
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index " + deleteElementIndex + " is Invalid");// O(1)
+        }
+        /**
+         * Time-Complexity :- O(1)
+         * Space-Complexity :- O(1)
+         */
     }
 }
-/**
- * Time-Complexity: O(1)
- */
