d70be7aaeefe7eeb520de042a44bba3ac957642d
Priyansusahoo <priyansusahoo1@gmail.com>
2022-10-09 18:17:57 +0530

    Added: Tererse 2D Array

--- Arrays/TwoDimensionalArray/Main.java
+++ Arrays/TwoDimensionalArray/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
         TwoDimensionalArray tda = new TwoDimensionalArray(3, 3);
         tda.insertValueInTheArray(0, 0, 10);
+        tda.insertValueInTheArray(0, 1, 20);
+        tda.insertValueInTheArray(1, 0, 30);
+        tda.insertValueInTheArray(2, 0, 40);
         // tda.insertValueInTheArray(0, 0, 40);
-        System.out.print(Arrays.deepToString(tda.arr));
-        tda.accessCell(0, 2);
+        // System.out.print(Arrays.deepToString(tda.arr));
+        // tda.accessCell(0, 2);
+        tda.traverse2DArray();
     }
 }
--- Arrays/TwoDimensionalArray/TwoDimensionalArray.java
+++ Arrays/TwoDimensionalArray/TwoDimensionalArray.java
@@ -14,14 +14,14 @@ public class TwoDimensionalArray {
     // Insertion value in the array
     public void insertValueInTheArray(int row, int col, int value) {
         try {
-            if (arr[row][col] == Integer.MIN_VALUE) { // O(1)
+            if (arr[row][col] == Integer.MIN_VALUE) { // TC: O(1)
                 arr[row][col] = value; // O(1)
-                System.out.println("Successfully inserted");// O(1)
+                System.out.println("Successfully inserted");// TC: O(1)
             } else {
-                System.out.println("Cell is already occupied");// O(1)
+                System.out.println("Cell is already occupied");// TC: O(1)
             }
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Invalid index for 2D Array");// O(1)
+            System.out.println("Invalid index for 2D Array");// TC: O(1)
         }
         /**
          * Time-Complexity:- O(1)
@@ -31,15 +31,33 @@ public class TwoDimensionalArray {
 
     // Accessing cell value from the given array
     public void accessCell(int row, int col) {
-        System.out.println("\nAccessing Row " + row + ", Col " + col); // O(1)
+        System.out.println("\nAccessing Row "
+                + row + ", Col "
+                + col); // O(1)
         try {
-            System.out.println("Cell value is " + arr[row][col]); // O(1)
+            System.out.println("Cell value is "
+                    + arr[row][col]); // O(1)
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid Index");// O(1)
         }
         /**
          * Time-Complexity: O(1)
          * Space-Complexity:- O(1)
+         */
+    }
+
+    // Traverse 2D Array
+    public void traverse2DArray() {
+        for (int row = 0; row < arr.length; row++) { // TC: O(m) if number of rows in 2D Array is 'm'
+            for (int col = 0; col < arr[row].length; col++) {// TC: O(n) if number of col in 2D Array is 'n'
+                System.out.print(arr[row][col]
+                        + " ");// TC: O(1)
+            }
+            System.out.println();// TC: O(1)
+        }
+        /**
+         * Time-Complexity:- O(mn)
+         * Space-Complexity:- O(1) -> no additional memory is required
          */
     }
 }
