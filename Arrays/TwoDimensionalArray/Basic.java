6c9fdb4e3421e62bbdc467eb22f2dabdf3f2b8af
Priyansusahoo <priyansusahoo1@gmail.com>
2022-10-09 14:01:39 +0530

    Added: TwoDimensionalArray/

--- Arrays/TwoDimensionalArray/Basic.java
+++ Arrays/TwoDimensionalArray/Basic.java
@@ -1,20 +1,32 @@
 import java.util.Arrays;
 
 /**
- * Main
+ * Basic
  */
-public class Main {
+public class Basic {
     public static void main(String[] args) {
         // Declaration
         int[][] int2DArray;
         // Instantiate
         int2DArray = new int[2][2];
-        // Initialization
+        // Initialization, Time-Complexity:- O(mn)
         int2DArray[0][0] = 1;
         int2DArray[0][1] = 2;
         int2DArray[1][0] = 3;
         int2DArray[1][1] = 4;
 
         System.out.println(Arrays.deepToString(int2DArray));
+
+        /**
+         * All together
+         * Declaration,Instantiate,Initialization
+         * Time-Complexity:- O(1)
+         */
+        String s2DArray[][] = { { "a", "b" }, { "c", "d" } };
+        System.out.println(Arrays.deepToString(s2DArray));
     }
 }
+
+/**
+ * Space-Complexity:- O(mn)
+ */
--- Arrays/TwoDimensionalArray/TwoDimensionalArray.java
+++ Arrays/TwoDimensionalArray/TwoDimensionalArray.java
@@ -1,3 +1,27 @@
 public class TwoDimensionalArray {
+    int arr[][] = null;
 
+    // constructor
+    public TwoDimensionalArray(int numberOdRows, int numberOfCloumns) {
+        this.arr = new int[numberOdRows][numberOfCloumns];
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr.length; col++) {
+                arr[row][col] = Integer.MIN_VALUE;
+            }
+        }
+    }
+
+    // Insertion value in the array
+    public void insertValueInTheArray(int row, int col, int value) {
+        try {
+            if (arr[row][col] == Integer.MIN_VALUE) {
+                arr[row][col] = value;
+                System.out.println("Successfully inserted");
+            } else {
+                System.out.println("cell is already occupied");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid index for 2D Array");
+        }
+    }
 }
