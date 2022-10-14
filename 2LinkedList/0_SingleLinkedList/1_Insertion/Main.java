35f6d9432297ba61ed35c4c2ff5263fae0361f04
priyansusahoo <priyansusahoo1@gmail.com>
2022-10-14 23:08:31 +0530

    Added: 2LinkedList/0_SingleLinkedList/1_Insertion/

---
+++ 2LinkedList/0_SingleLinkedList/1_Insertion/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main
+ */
+class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        sLL.createSinglyLinkedList(5);
+        System.out.println(sLL.head.value);
+    }
+}
---
+++ 2LinkedList/0_SingleLinkedList/1_Insertion/SinglyLinkedList.java
@@ -0,0 +1,19 @@
+/**
+ * SinglyLinkedList
+ */
+public class SinglyLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node createSinglyLinkedList(int nodeValue) {
+        head = new Node();
+        Node node = new Node();
+        node.nextReference = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+}
