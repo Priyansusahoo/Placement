1d271106603f5150512e43b53d260298fa26df27
Priyansu <priyansusahoo1@gmail.com>
2022-10-28 10:57:09 +0530

    added: insertCSLL() for insertion of node in CSLL
    modified:
    -> 1_CircularSinglyLinkedList/0_Creation/CSLL/src/CircularSinglyLinkedList.java
    -> 1_CircularSinglyLinkedList/0_Creation/CSLL/src/Main.java
    
    CSLL: circular Singly Linked List

--- 2LinkedList/1_CircularSinglyLinkedList/0_Creation/CSLL/src/CircularSinglyLinkedList.java
+++ 2LinkedList/1_CircularSinglyLinkedList/0_Creation/CSLL/src/CircularSinglyLinkedList.java
@@ -3,7 +3,9 @@ public class CircularSinglyLinkedList {
     public Node tail;
     public int size;
 
-    //creation
+    /**
+     * creation
+     */
     public Node createCSLL(int nodeValue){
         head = new Node();
         Node node = new Node();
@@ -13,5 +15,38 @@ public class CircularSinglyLinkedList {
         tail = node;
         size = 1;
         return head;
+    }
+    /**
+     * insertion
+     */
+    public void insertCSLL(int nodeValue, int location){
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null){ // LL exist or not -----------------------------> O(1)
+            createCSLL(nodeValue);
+            return;
+        } else if (location == 0) { // at beginning ------------------------> O(1)
+            node.next = head;
+            head = node;
+            tail.next = head;
+        } else if (location >= size) { // at last ---------------------------> O(1)
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        } else { // at mentioned location
+            Node tempNode = head;
+            int index = 0;
+            while(index < location - 1){ //refer to lecture 101 ---------------> O(N)
+                tempNode = tempNode.next;
+                index ++;
+            }
+            node.next = tempNode.next;
+            tempNode.next = node;
+        }
+        size ++;
+        /**
+         * Time-Complexity: O(N)
+         * Space-Complexity: O(1)
+         */
     }
 }
--- 2LinkedList/1_CircularSinglyLinkedList/0_Creation/CSLL/src/Main.java
+++ 2LinkedList/1_CircularSinglyLinkedList/0_Creation/CSLL/src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         CircularSinglyLinkedList cSLL = new CircularSinglyLinkedList();
         cSLL.createCSLL(5);
+        cSLL.insertCSLL(4,0);
+        cSLL.insertCSLL(3,1);
+        cSLL.insertCSLL(7,8);
         System.out.println(cSLL.head.value);
-        System.out.println(cSLL.head.next.value);
+        System.out.println(cSLL.head.next.next.value);
         /**
          * Time-Complexity: O(1)
          * Space-Comlpexity: O(1)
