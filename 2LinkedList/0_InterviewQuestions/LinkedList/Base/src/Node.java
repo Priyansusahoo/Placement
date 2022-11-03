60e9c3e8165134240803263fffa4bd4187a359b6
Priyansu <priyansusahoo1@gmail.com>
2022-11-03 14:27:30 +0530

    added: RemoveDuplicates & LeetCode-206
    details:
    -> solution of LeetCode-206
    -> added a base class for linked list
    -> added RemoveDuplicates

---
+++ 2LinkedList/0_InterviewQuestions/LinkedList/Base/src/Node.java
@@ -0,0 +1,5 @@
+public class Node {
+    public Node prev;
+    public int value;
+    public Node next;
+}
---
+++ 2LinkedList/0_InterviewQuestions/RemoveDuplicates/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.createLL(1);
+        ll.insertNode(2);
+        ll.insertNode(3);
+        ll.insertNode(4);
+        ll.insertNode(5);
+        ll.traversalLL();
+    }
+}
