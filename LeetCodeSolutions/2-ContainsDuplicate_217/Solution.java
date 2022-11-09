80954a117c4446bf4cff3bcf9271832ad2e08ac9
Priyansu <priyansusahoo1@gmail.com>
2022-11-09 15:47:38 +0530

    LeetCode DAILY
    Details:
    -> 2-ContainsDuplicate_217/
    -> 4-TwoSum_1/
    -> 7-Reverse-Linked-List_206/

--- LeetCodeSolutions/2-ContainsDuplicate_217/Solution.java
+++ LeetCodeSolutions/2-ContainsDuplicate_217/Solution.java
@@ -1,12 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
+        //Method - 1
         HashSet<Integer> numbers = new HashSet<>();
         
         for(int i=0;i<nums.length;i++){
             if(numbers.contains(nums[i])) return true;
             numbers.add(nums[i]);
+         }
+           return false;
+        }
+       
+       //Method - 2
+       /**
+       Arrays.sort(nums);
+        
+        for (int i = 1; i< nums.length; i++) {
+            if (nums[i] == nums[i-1]){
+                return true;
+            }
         }
         return false;
-    }
+        */
 }
--- LeetCodeSolutions/4-TwoSum_1/Solution.java
+++ LeetCodeSolutions/4-TwoSum_1/Solution.java
@@ -1,4 +1,5 @@
 class Solution {
+    //method - 1
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -9,4 +10,21 @@ class Solution {
         }
         return null;
     }
+    //method - 2
+    /**
+    public int[] twoSum(int[] nums, int target) {
+        int[] output = new int[2];
+        for (int i = 0; i< nums.length; i++) {
+            for (int j = i+1; j< nums.length; j++) {
+                if(nums[j] == target - nums[i]) {
+                    // return new int[] {i, j};
+                    output[0] = i;
+                    output[1] = j;
+                    return output;
+                }
+            }
+        }
+        return output;
+    }
+    */
 }
