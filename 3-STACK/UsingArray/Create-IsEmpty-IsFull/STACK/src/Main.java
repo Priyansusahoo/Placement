37fbd38782ca16daea8891bd0207feb4d5db8b2b
Priyansusahoo <priyansusahoo1@gmail.com>
2022-11-25 15:52:40 +0530

    added: Stack Using Arrays
    details:
    added:-
    - create()
    - isEmpty()
    - isFull()
    - push()
    - pop()
    - peek()
    - delete()

--- 3-STACK/UsingArray/Create-IsEmpty-IsFull/STACK/src/Main.java
+++ 3-STACK/UsingArray/Create-IsEmpty-IsFull/STACK/src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         Stack newStack = new Stack(4);
+        //push
+        newStack.push(2);
+        newStack.push(3);
+        newStack.push(4);
+        newStack.push(5);
+        //Pop
+        int popElement = newStack.pop();
+        System.out.println("Element Poped is " + popElement);
+        //isEmpty
         boolean resultEmpty = newStack.isEmpty();
-        System.out.println(resultEmpty);
+        System.out.println("Is Empty: " + resultEmpty);
+        //isFull
         boolean resultFull = newStack.isFull();
-        System.out.println(resultFull);
+        System.out.println("Is Full: " + resultFull);
+        //Peek
+        int peekElement = newStack.peek();
+        System.out.println("Top element is: " + peekElement);
+        // check Peek
+        int peekElement1 = newStack.peek();
+        System.out.println("Top element CHECK is: " + peekElement1);
+        //Delete
+        newStack.delete();
     }
 }
--- 3-STACK/UsingArray/Create-IsEmpty-IsFull/STACK/src/Stack.java
+++ 3-STACK/UsingArray/Create-IsEmpty-IsFull/STACK/src/Stack.java
@@ -33,11 +33,70 @@ public class Stack {
      */
     public boolean isFull() {
         if (topOfStack == arr.length - 1) {
-            System.out.println("is full");
+//            System.out.println("is full");
             return true;
         } else {
             return false;
         }
+        /**
+         * Time-Complexity: O(1)
+         * Space-Complexity: O(1)
+         */
+    }
+    /**
+     * Push
+     */
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full...insertion unsuccessful");
+        } else {
+            arr[topOfStack+1] = value;
+            topOfStack ++;
+            System.out.println("Successfully inserted");
+        }
+        /**
+         * Time-Complexity: O(1)
+         * Space-Complexity: O(1)
+         */
+    }
+    /**
+     * Pop
+     */
+    public int pop() {
+        if (isEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        } else {
+            int topElement = arr[topOfStack];
+            topOfStack--;
+            return topElement;
+        }
+        /**
+         * Time-Complexity: O(1)
+         * Space-Complexity: O(1)
+         */
+    }
+    /**
+     * Peek
+     */
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return -1;
+        } else {
+            return arr[topOfStack];
+        }
+        /**
+         * Time-Complexity: O(1)
+         * Space-Complexity: O(1)
+         */
+    }
+    /**
+     * Delete
+     */
+    public void delete() {
+        arr = null;
+        System.out.println("Successfully deleted the STACK :D");
         /**
          * Time-Complexity: O(1)
          * Space-Complexity: O(1)
